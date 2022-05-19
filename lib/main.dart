2397e6e8ad2b0756cbb8e0837ccf2f7aafddc16f
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-05-19 22:39:46 +0100

    Center image

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 //the main function starting point of our apps
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title:  const Text('I am Rich'),
+        title: const Text('I am Rich'),
         centerTitle: true,
         backgroundColor: Colors.blueGrey[900],
         foregroundColor: Colors.white,
       ),
-      backgroundColor: Colors.green[300],
-      body: Image(image: NetworkImage('https://www.w3schools.blog/wp-content/uploads/2019/10/word-image-155.png'),),
-
+      backgroundColor: Color.fromARGB(255, 156, 185, 157),
+      body: SizedBox(
+          child: Center(
+              child: Image(
+        image: NetworkImage(
+            'https://www.w3schools.blog/wp-content/uploads/2019/10/word-image-155.png'),
+      ))),
     ),
   ));
 }
